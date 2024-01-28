608f0aadb470368172ed2e256ca4f80dc4e10b36
kamish13 <78052474+kamish13@users.noreply.github.com>
2024-01-28 15:39:31 +0100

    update typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,19 @@
+import { registerAs } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+const config: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.NODE_ENV === 'production' ? 'db' : '127.0.0.1', // Use 'db' in production, '127.0.0.1' locally
   port: 5432,
   username: 'postgres',
   password: 'postgres',
   database: 'postgres',
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
+  synchronize: true,
 };
+
+export default registerAs('typeorm', () => config);
+export const connectionSource = new DataSource(config as DataSourceOptions);
